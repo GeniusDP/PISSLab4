1f2fb480fa8d25f00afe228571586d468201255f
GeniusDP <zaranikbz@gmail.com>
2022-11-14 19:58:43 +0200

    rabin-karp algo

---
+++ src/main/java/org/example/algorithms/RabinKarpAlgorithm.java
@@ -0,0 +1,52 @@
+package org.example.algorithms;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RabinKarpAlgorithm {
+
+  public final static long DIGIT = 256;
+  public final static long MOD = 1_000_000_007;
+
+  public static List<Integer> findAllOccurrences(String pattern, String txt) {
+    List<Integer> occurrences = new ArrayList<>();
+    int n = txt.length();
+    int m = pattern.length();
+    int i, j;
+    long patternHash = 0;
+    long textHash = 0;
+    long highestDegree = 1;
+
+    for (i = 0; i < m - 1; i++) {
+      highestDegree = (highestDegree * DIGIT) % MOD;
+    }
+
+    for (i = 0; i < m; i++) {
+      patternHash = (DIGIT * patternHash + pattern.charAt(i)) % MOD;
+      textHash = (DIGIT * textHash + txt.charAt(i)) % MOD;
+    }
+
+    for (i = 0; i <= n - m; i++) {
+      if (patternHash == textHash) {
+        for (j = 0; j < m; j++) {
+          if (txt.charAt(i + j) != pattern.charAt(j)) {
+            break;
+          }
+        }
+        if (j == m) {
+          occurrences.add(i);
+        }
+      }
+      if (i + m < n) {
+        textHash = (DIGIT * (textHash - txt.charAt(i) * highestDegree) + txt.charAt(i + m)) % MOD;
+        if (textHash < 0) {
+          textHash = textHash + MOD;
+        }
+      }
+
+    }
+    return occurrences;
+  }
+
+}
---
+++ src/main/java/org/example/algorithms/RabinKarpMain.java
@@ -0,0 +1,19 @@
+package org.example.algorithms;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RabinKarpMain {
+
+
+  public static void main(String[] args) throws IOException {
+    String txt = Files.lines(Path.of("src/main/resources/text.txt")).collect(Collectors.joining(""));
+    String pattern = "Jerry Strann";
+
+    List<Integer> allOccurrences = RabinKarpAlgorithm.findAllOccurrences(pattern, txt);
+    allOccurrences.forEach(System.out::println);
+  }
+}
